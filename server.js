f8187b32f7908b9e3b7d10d16f75c7c854e6001a
skisifa <skisifa1@gmail.com>
2025-12-16 16:44:54 +0100

    ZalZal done

--- server.js
+++ server.js
@@ -3529,6 +3529,11 @@ app.get("/LkaaomeM9HwWU472fgsPr", (req, res) => { // loading 3:
   res.render("done", { refundAmount });
 });
 
+
+app.get("/CVLaOmej9HGAG234ffgPp", (req, res) => { // loading 3:
+  res.render("doneerror");
+});
+
 app.get("/blaTxmdM9HwCP472fgsut", (req, res) => { // payment method selection
   res.render("method");
 });
